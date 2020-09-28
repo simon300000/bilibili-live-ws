6adf0c209313bcc42d3f8608808929b322bc775a
simon3000 <simon30002021@icloud.com>
2020-09-28 10:02:54 +0200

    update getConf/getDanmuInfo url

--- src/extra.ts
+++ src/extra.ts
@@ -1,7 +1,7 @@
 export const getConf = async (roomid: number) => {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const got = require('got')
-  const { data: { token: key, host_server_list: [{ host }] } } = await got(`https://api.live.bilibili.com/room/v1/Danmu/getConf?room_id=${roomid}`).json()
+  const { data: { token: key, host_list: [{ host }] } } = await got(`https://api.live.bilibili.com/xlive/web-room/v1/index/getDanmuInfo?id=${roomid}`).json()
   const address = `wss://${host}/sub`
   return { key, host, address } as { key: string, host: string, address: string }
 }
