b008d68f9c945842594b871d64d47e1cb2a84871
simon3000 <w@simon3k.moe>
2021-12-26 03:27:39 +0800

    use google brotli

--- src/inflate/browser.ts
+++ src/inflate/browser.ts
@@ -1,9 +1,9 @@
 import { Buffer } from 'buffer'
 
-import decompression from 'brotli/decompress'
+import { BrotliDecode } from './brotli'
 import { inflate } from 'pako'
 
 const inflateAsync = (d: Buffer) => Buffer.from(inflate(d))
-const brotliDecompressAsync = (d: Buffer) => Buffer.from(decompression(d))
+const brotliDecompressAsync = (d: Buffer) => Buffer.from(BrotliDecode(Int8Array.from(d)))
 
 export const inflates = { inflateAsync, brotliDecompressAsync }
