05f5f998d1da13314b43a7320cd16d551d682c59
simon3000 <simon30002021@icloud.com>
2023-09-13 22:29:23 +0200

    getConf with type

--- src/extra.ts
+++ src/extra.ts
@@ -1,8 +1,29 @@
+type GET_DANMU_INFO = {
+  code: number
+  message: string
+  ttl: number
+  data: {
+    business_id: number
+    group: string
+    host_list: {
+      host: string
+      port: number
+      wss_port: number
+      ws_port: number
+    }[]
+    max_delay: number
+    refresh_rate: number
+    refresh_row_factor: number
+    token: string
+  }
+}
+
 export const getConf = async (roomid: number) => {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const { data: { token: key, host_list: [{ host }] } } = await fetch(`https://api.live.bilibili.com/xlive/web-room/v1/index/getDanmuInfo?id=${roomid}`).then(w => w.json())
+  const raw = await fetch(`https://api.live.bilibili.com/xlive/web-room/v1/index/getDanmuInfo?id=${roomid}`).then(w => w.json()) as GET_DANMU_INFO
+  const { data: { token: key, host_list: [{ host }] } } = raw
   const address = `wss://${host}/sub`
-  return { key, host, address } as { key: string, host: string, address: string }
+  return { key, host, address, raw }
 }
 
 export const getRoomid = async (short: number) => {
